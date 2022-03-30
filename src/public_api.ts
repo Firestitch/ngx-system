4d129f0244fa9b6cbdf9c5df6be946b0ee08e22e
Eugene Kubesh <codbasters@gmail.com>
2022-03-30 21:22:07 +0300

    update to ng12

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,5 +1,21 @@
 export { FsSystemModule } from './app/fs-system.module';
 
+export { DashboardComponent } from './app/components/dashboard';
+export { CronsComponent } from './app/components/crons';
+export { SettingsComponent } from './app/components/settings';
+export { KeyNameValuePipe } from './app/pipes/key-name-value.pipe';
+export { FileManagerComponent } from './app/components/file-manager';
+export { ProcessesComponent } from './app/components/processes';
+export { ServerLogComponent } from './app/components/server-log';
+export { ServerLogsComponent } from './app/components/server-logs';
+export { UpgradeLogsComponent } from './app/components/upgrade-logs';
+export { ApiLogsComponent } from './app/components/api-logs';
+export { ApiLogComponent } from './app/components/api-log';
+export { InfoComponent } from './app/components/info';
+export { ProcessComponent } from './app/components/process';
+export { CronComponent } from './app/components/cron';
+export { CronLogComponent } from './app/components/cron-log';
+
 export { ApiLogState, CronState, ProcessState } from './app/enums';
 export { ApiLogStates, CronStates, ProcessStates } from './app/consts';
 
